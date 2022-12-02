9d1c6302463ed08cfccf3f2f4bc804fc1adc884f
kroles1 <100869848+kroles1@users.noreply.github.com>
2022-12-02 15:57:10 +0000

    register page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Register() {
+  const [formData, setFormData] = useState({ username: "", email: "", password: "" })
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch()
+    navigate("/")
+  }
+
+  const handleChange = (e) => {
+    const { name, value } =e.target
+    setFormData({...formData, [name]: value});
+  }
+
   return (
     <>
-    <form>
-      <input type="text" placeholder='Username'></input>
-      <input type="password" placeholder='Password'></input>
-      <input type="password" placeholder='Confirm password'></input>
-      <input type="text" placeholder='Email'></input>
-      <input type="submit" value='Register'></input>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Username" name="username" value={formData.username} onChange={handleChange}></input>
+      <input type="email" placeholder="Email" name="email" value={formData.email} onChange={handleChange}></input>
+      <input type="password" placeholder="Password" name="password" value={formData.password} onChange={handleChange}></input>
+      <input type="password" placeholder="Confirm password" name="confirm_password" value={formData.password} onChange={handleChange}></input>
+      <input type="submit" value="Register"></input>
     </form>
     </>
   )
