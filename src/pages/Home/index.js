0d450dc01ee353febb3b504bad14e24ec598c82d
kroles1 <100869848+kroles1@users.noreply.github.com>
2022-12-05 09:37:16 +0000

    login and register page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,33 +1,48 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import './style.css'
 
 
 export default function Home() {
-  const [formData, setFormData] = useState({ username: "", password: "" })
+  // const [formData, setFormData] = useState({ username: "", password: "" })
+  const [logIn, setLogIn] = useState(null);
+  const getFormData = (form) => Object.fromEntries(new FormData(form).entries());
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch()
-    navigate("/dashboard")
+		const formData = getFormData(e.target);
+		axios.post("http://127.0.0.1:5000/login", formData)
+			.then((res) => {
+				console.log(res.data, "POST RESPONSE");
+				if (res.data.message === "Welcome Panini Player") {
+					console.log("USERNAME:", formData["username"]);
+					setLogIn(formData);
+          navigate("/dashboard")
+				} else {
+					console.log("FAILED");
+				}
+			})
+			.catch((err) => console.error(err));
   }
-
-  const handleChange = (e) => {
-    const { name, value } =e.target
-    setFormData({...formData, [name]: value});
-  }
-
+  // const handleChange = (e) => {
+  //   const { name, value } =e.target
+  //   setFormData({...formData, [name]: value});
+  // }
   return (
     <>
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Username" name="username" value={formData.username} onChange={handleChange}></input>
-      <input type="password" placeholder="Password" name="password" value={formData.password} onChange={handleChange}></input>
-      <input type="submit" value="Log in"></input>
+    <div className='loginpg'>
+    <form className='login' onSubmit={handleSubmit}>
+      <input className="formInput" type="text" placeholder="Username" name="username"></input>
+      <input className="formInput" type="password" placeholder="Password" name="password"></input>
+      <input className="submitBtn" type="submit" value="Log in"></input>
     </form>
-    <button>Register</button>
+    <button className="registerNav"><a href='/register'>Register</a></button>
+    </div>
     </>
   )
 }
