280b83f70a392144b83bb50a4befd90e0cc00fcf
Brendan-Geoghegan <brendangeoghegan@outlook.com>
2022-12-07 18:21:31 +0000

    sticker code appears on stickers you don't have

--- src/components/Sticker/index.js
+++ src/components/Sticker/index.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Sticker({name, image, stickerId}) {
+export default function Sticker({name, image, stickerId, rarity, owned}) {
+  const [showStars, setShowStars] = useState(false)
+
+  const renderRarity = () => {
+    const array = new Array(rarity)
+    array.fill(1)
+    console.log(array);
+    return(
+      array.map(star => {
+        return <li><img className='stars' src="https://pngimg.com/uploads/star/star_PNG41513.png" style={showStars ? {visibility: "visible"} : {visibility: "hidden"}} /></li>
+      })
+    )
+  }
+
   return (
-    <div>
+    <div onMouseEnter={() => setShowStars(true)} onMouseLeave={() => setShowStars(false)}>
         {/* <h2>Name: {name}</h2> */}
         <img src={image} alt='Picture of sticker' />
-        <p>Sticker id: {stickerId}</p>
+        <p style={!owned ? {visibility: "visible"} : {visibility: "hidden"}} className='stickerId'>{stickerId}</p>
+        <ul className='starsContainer'>
+          {renderRarity()}
+        </ul>
     </div>
   )
 }
