371e322a7e93bbb28701db1c8c4ac5ac2315b89e
Brendan-Geoghegan <brendangeoghegan@outlook.com>
2022-12-08 15:50:55 +0000

    more styling

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <div className='navbar'>
       <NavLink to="/dashboard/profile" className='navlink'>Profile</NavLink>
-      <NavLink to="" className='navlink'>Album</NavLink>
+      <NavLink to="/dashboard/album" className='navlink'>Album</NavLink>
       <NavLink to="/dashboard/friends" className='navlink'>Friend List</NavLink>
       <NavLink to="/dashboard/public" className='navlink'>Public Trades</NavLink>
     </div>
